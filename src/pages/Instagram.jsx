784428043d1b327fe4e2498e5dbde61f3e5aecfe
Kathleen Wayman <katway17@gmail.com>
2025-03-13 17:15:00 -0600

    Updated Instagram page

--- src/pages/Instagram.jsx
+++ src/pages/Instagram.jsx
@@ -4,7 +4,7 @@ export default function Instagram() {
   return (
     <div className="instagram-page">
       <div className="instagram-container">
-        <h1>📖 My Instagram Feed</h1>
+        <h1>My Instagram Profile</h1>
         <p>Check out my latest bookstagram posts!</p>
 
         {/* 📸 Instagram Profile Embed */}
